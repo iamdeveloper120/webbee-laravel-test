8f759e47e62c835d38783dcf614c6794ce831abb
Muhammad Farhan <itianz@outlook.com>
2023-05-01 09:58:40 -0700

    Task 1: getEventsWithWorkshops completed with one query

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,15 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
+  /**
+   * Get the comments for the blog post.
+   */
+  public function workshops(): HasMany
+  {
+    return $this->hasMany(Workshop::class);
+  }
 }
