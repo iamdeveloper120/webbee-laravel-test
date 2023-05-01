7d20bcc463f4d3eeaa58733130d16eccc695bcba
Muhammad Farhan <itianz@outlook.com>
2023-05-01 10:55:49 -0700

    Task 2: completed getFutureEventWithWorkshops

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Database\Eloquent\Builder;
 
 class EventsController extends BaseController
 {
@@ -182,6 +184,8 @@ class EventsController extends BaseController
 
     public function getFutureEventsWithWorkshops()
     {
-        throw new \Exception('implement in coding task 2');
+        Event::with('workshops')->whereHas('workshops', function ($q) {
+            $q->where('start', '>', now());
+        })->get();
     }
 }
